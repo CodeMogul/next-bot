cd26ddb7c71f4892758a39c4e86d28ff1791932c
Siddhesh Nachane <siddheshn@next-hyd-659.nexteducation.com>
2017-03-27 17:08:58 +0530

    added PID for straight line motion

--- NextBotMotors/NextBotMotors.cpp
+++ NextBotMotors/NextBotMotors.cpp
@@ -6,6 +6,9 @@ NextBotMotors::NextBotMotors(uint8_t mL_dirA, uint8_t mL_dirB, uint8_t mL_pwm,
     _mR_dirA(mR_dirA), _mR_dirB(mR_dirB), _mR_pwm(mR_pwm)
 {
     setOutputLimits(0, 255);
+    _mL_encAdd = _mR_encAdd = 1;
+    _mL_encCount = _mR_encCount = 0;
+    _moving = false;
 }
 
 void NextBotMotors::begin()
@@ -13,16 +16,35 @@ void NextBotMotors::begin()
     pinMode(_mL_dirA, OUTPUT);
     pinMode(_mL_dirB, OUTPUT);
     pinMode(_mR_dirA, OUTPUT);
-    pinMode(_mR_dirB, OUTPUT);
+    pinMode(_mR_dirB, OUTPUT);  
+    _encEnable = false;  
 }
 
-void NextBotMotors::setOutputLimits(uint16_t Min, uint16_t Max)
+void NextBotMotors::begin(float Kp, float Ki, float Kd)
 {
-    if (Min >= Max)
+    begin();
+    _encEnable = true;
+    _updateTime = 100;
+    _iTerm = 0;
+    setTunings(Kp, Ki, Kd);
+    _lastUpdate = millis();
+}
+
+void NextBotMotors::setOutputLimits(uint16_t min, uint16_t max)
+{
+    if (min >= max)
         return;
 
-    _outMin = Min;
-    _outMax = Max;
+    _outMin = min;
+    _outMax = max;
+}
+
+void NextBotMotors::setTunings(float kp, float ki, float kd)
+{
+    float updateTimeInSec = (float)_updateTime/1000;
+    _kp = kp;
+    _ki = ki * updateTimeInSec;
+    _kd = kd / updateTimeInSec;
 }
 
 void NextBotMotors::_setLeftMotorDir(uint8_t dir)
@@ -32,10 +54,12 @@ void NextBotMotors::_setLeftMotorDir(uint8_t dir)
         case FORWARD:
             digitalWrite(_mL_dirA, HIGH);
             digitalWrite(_mL_dirB, LOW);
+            _mL_encAdd = 1;
             break;
         case REVERSE:
             digitalWrite(_mL_dirA, LOW);
             digitalWrite(_mL_dirB, HIGH);
+            _mL_encAdd = -1;
             break;
         case STOP:
             digitalWrite(_mL_dirA, LOW);
@@ -55,10 +79,12 @@ void NextBotMotors::_setRightMotorDir(uint8_t dir)
         case FORWARD:
             digitalWrite(_mR_dirA, HIGH);
             digitalWrite(_mR_dirB, LOW);
+            _mR_encAdd = 1;
             break;
         case REVERSE:
             digitalWrite(_mR_dirA, LOW);
             digitalWrite(_mR_dirB, HIGH);
+            _mR_encAdd = -1;
             break;
         case STOP:
             digitalWrite(_mR_dirA, LOW);
@@ -73,27 +99,82 @@ void NextBotMotors::_setRightMotorDir(uint8_t dir)
 
 void NextBotMotors::move(uint8_t dir, uint8_t velocity)
 {
-    velocity = map(velocity, 0, 100, _outMin, _outMax);
+    if(dir == STOP || dir == BRAKE) _mL_out = 0;
+    else _mL_out = map(velocity, 0, 100, _outMin, _outMax);
+    _iTerm = _mR_out = _mL_out;
 
     _setLeftMotorDir(dir);
     _setRightMotorDir(dir);
 
-    analogWrite(_mL_pwm, velocity);
-    analogWrite(_mR_pwm, velocity);
+    analogWrite(_mL_pwm, _mL_out);
+    analogWrite(_mR_pwm, _mR_out);
+
+    _moving = true;
 }
 
-void NextBotMotors::moveLeftMotor(uint8_t dir, uint8_t velocity)
+void NextBotMotors::stop()
 {
-    velocity = map(velocity, 0, 100, _outMin, _outMax);
+    _setLeftMotorDir(STOP);
+    _setRightMotorDir(STOP);
+
+    analogWrite(_mL_pwm, 0);
+    analogWrite(_mR_pwm, 0);
+
+    _moving = false;
+}
+
+void NextBotMotors::brake()
+{
+    _setLeftMotorDir(BRAKE);
+    _setRightMotorDir(BRAKE);
+
+    analogWrite(_mL_pwm, 0);
+    analogWrite(_mR_pwm, 0);
+
+    _moving = false;
+}
+
+void NextBotMotors::leftMotor(uint8_t dir, uint8_t velocity)
+{
+    if(dir == STOP || dir == BRAKE) _mL_out = 0;
+    else _mL_out = map(velocity, 0, 100, _outMin, _outMax);
+
     _setLeftMotorDir(dir);
-    analogWrite(_mL_pwm, velocity);
+    analogWrite(_mL_pwm, _mL_out);
 }
 
-void NextBotMotors::moveRightMotor(uint8_t dir, uint8_t velocity)
+void NextBotMotors::rightMotor(uint8_t dir, uint8_t velocity)
 {
-    velocity = map(velocity, 0, 100, _outMin, _outMax);
+    if(dir == STOP || dir == BRAKE) _mR_out = 0;
+    else _mR_out = map(velocity, 0, 100, _outMin, _outMax);
+
     _setRightMotorDir(dir);
-    analogWrite(_mR_pwm, velocity);
+    analogWrite(_mR_pwm, _mR_out);
 }
 
+bool NextBotMotors::updateState()
+{
+    uint32_t now = millis();
+    if(!_moving || !_encEnable || (now - _lastUpdate) < _updateTime) return false;
+    
+    int32_t input = _mL_encCount, setPoint = _mR_encCount;
 
+    int16_t error = setPoint - input;
+    _iTerm += _ki * error;
+    if(_iTerm > _outMax) _iTerm = _outMax;
+    if(_iTerm < _outMin) _iTerm = _outMin;
+    float dInput = input - _lastInput;
+
+    float output = _kp * error + _iTerm + (_kd * dInput);
+
+    if(output > _outMax) output = _outMax;
+    if(output < _outMin) output = _outMin;
+    
+    _mL_out = output;
+    analogWrite(_mR_pwm, _mL_out);
+
+    _lastInput = input;
+    _lastUpdate = now;
+
+    return true;
+}
--- NextBotMotors/NextBotMotors.h
+++ NextBotMotors/NextBotMotors.h
@@ -11,45 +11,72 @@
 class NextBotMotors 
 {
     public:
-        NextBotMotors(uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t);
+        NextBotMotors(uint8_t mL_dirA, uint8_t mL_dirB, uint8_t mL_pwm,
+                    uint8_t mR_dirA, uint8_t mR_dirB, uint8_t mR_pwm);
 
-        /** begin() functions needs to be called to initiate hardware pins. */
+        /** begin() functions needs to be called to initiate hardware pins. 
+         *
+         *  @param encEnable Boolean value to enable encoder based calculations.
+         */
         void begin();
 
+        void begin(float Kp, float Ki, float Kd);
+
         /** Sets the minimum and maximum output limit of PWM pins.*/
-        void setOutputLimits(uint16_t, uint16_t);
+        void setOutputLimits(uint16_t min, uint16_t max);
 
         /** move() function for movement of the robot at a given speed.
          *   
          *  @param dir  Direction of movement (value can be FORWARD or REVERSE).
          *  @param speed Speed of motor between 0-100
          */
-        void move(uint8_t, uint8_t);
-
-        void moveLeftMotor(uint8_t dir, uint8_t velocity);
-
-        void moveRightMotor(uint8_t dir, uint8_t velocity);
-
+        void move(uint8_t dir, uint8_t velocity);
         void brake();
-
         void stop();
+        void leftMotor(uint8_t dir, uint8_t velocity);
+        void rightMotor(uint8_t dir, uint8_t velocity);
     
+        /** Updates left motor encoder count.
+         *  
+         *  Call this function in function attached to the hardware interrupt pin.
+         */
+        void updateL_enc()  { _mL_encCount += _mL_encAdd; }
+
+        /** Updates right motor encoder count.
+         *  
+         *  Call this function in function attached to the hardware interrupt pin.
+         */
+        void updateR_enc()  { _mR_encCount += _mR_encAdd; }
+
+        void setTunings(float kp, float ki, float kd);
+
+        bool updateState();
+
     private:
+        bool _moving;
         uint8_t _mL_dirA, _mL_dirB, _mL_pwm;
         uint8_t _mR_dirA, _mR_dirB, _mR_pwm;
+        uint16_t _mL_out, _mR_out;
         uint16_t _outMin, _outMax;
+
+        bool _encEnable;
+        volatile int32_t _mL_encCount, _mR_encCount;
+        int8_t _mL_encAdd, _mR_encAdd;
+        uint32_t _lastUpdate, _updateTime, _lastInput;
+        float _iTerm;
+        float _kp, _ki, _kd;
 
         /** Sets the direction of Left Motor.
          * 
          *  @param dir Direction, values can be (FORWARD, REVERSE, STOP, BRAKE)
          */
-        void _setLeftMotorDir(uint8_t);
+        void _setLeftMotorDir(uint8_t dir);
 
         /** Sets the direction of Right Motor.
          * 
          *  @param dir Direction, values can be (FORWARD, REVERSE, STOP, BRAKE)
          */
-        void _setRightMotorDir(uint8_t);
+        void _setRightMotorDir(uint8_t dir);
 };
 
 #endif
